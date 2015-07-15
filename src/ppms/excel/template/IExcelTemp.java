ce1054dc4f33f56060a157c2e58db79d1052f527
SharksevenRo <937257166@qq.com>
2015-07-15 16:29:48 +0800

    冲突测试

--- src/ppms/excel/template/IExcelTemp.java
+++ src/ppms/excel/template/IExcelTemp.java
@@ -9,5 +9,17 @@ import ppms.dao.BaseDao;
  */
 public interface IExcelTemp {
 
+	/**
+	 * 向数据库存入excel的数据
+	 * @param dao
+	 * @return
+	 */
 	public IExcelTemp toSave(BaseDao dao); 
+	
+	/**
+	 * 当前对象转换成excel表的记录
+	 * @return
+	 */
+	public IExcelTemp toExcel();
+	
 }
