4c70888bf013e720ec1d50844c721978b35b2c13
SharksevenRo <937257166@qq.com>
2015-07-15 14:11:24 +0800

    Update UserDaoImp.java

--- src/ppms/daoimpl/UserDaoImp.java
+++ src/ppms/daoimpl/UserDaoImp.java
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 package ppms.daoImpl;
-=======
-package ppms.daoimpl;
->>>>>>> 26d1baed399ee2a111e8f775f2857a8b903a1b08
-
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
