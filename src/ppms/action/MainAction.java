8b85d73541cb3dc1dd4d5e5c9a62804705be6f94
SharksevenRo <937257166@qq.com>
2015-07-29 15:44:36 +0800

    login

--- src/ppms/action/MainAction.java
+++ src/ppms/action/MainAction.java
@@ -1,12 +1,15 @@
 package ppms.action;
 
 import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.ResultPath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import ppms.serviceimpl.UserServiceImp;
 
 @Controller
+
 public class MainAction {
 
 
@@ -25,6 +28,15 @@ public class MainAction {
 		}finally{
 			return "success";
 		}
+		
+	}
+	
+	@Action(value ="login", results = {  
+	        @Result(name = "success", location = "/WEB-INF/content/page/userinfo/Demo.jsp"),  
+	        @Result(name = "faild", location="/WEB-INF/content/error.jsp")})
+	public String login(){
+		
+		return "success";
 		
 	}
 }
