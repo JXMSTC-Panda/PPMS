0c10532baaf8e47936118f1ec9df67df0af945a9
SharksevenRo <937257166@qq.com>
2015-08-13 08:22:23 +0800

    ToJsonUtil优化

--- src/ppms/util/ToJsonUtil.java
+++ src/ppms/util/ToJsonUtil.java
@@ -27,7 +27,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 /**
- * json转换类
+ * json转换类,实现成员变量内有代理对象的转换在查找和一些代理集合的设null操作后转换
  * @author shark
  * @update 2015下午6:29:38
  * @function
@@ -44,10 +44,6 @@ public class ToJsonUtil {
 	 */
 	private Class<?> clazz;
 	/**
-	 * 查询语句
-	 */
-	private String hsql;
-	/**
 	 * json根名
 	 */
 	private String rootName;
@@ -63,7 +59,6 @@ public class ToJsonUtil {
 	 * @param clazz
 	 */
 	
-	private static Set<String> classNames;
 	public void setFieldToJson(String[] fieldNames) {
 
 		this.fieldNames = fieldNames;
@@ -77,33 +72,9 @@ public class ToJsonUtil {
 	
 	public ToJsonUtil(){
 		String packageName = "ppms.domain";
-		classNames = getClassName(packageName, false);
 	}
-	public void setHsql(String hsql, boolean flag) {
-		this.hsql = hsql;
-		this.flag = flag;
-	}
-
-	public void setHsql(String hsql) {
-		this.hsql = hsql;
-		this.flag = false;
-	}
-
 	public void setKey(String rootName) {
 		this.rootName = rootName;
-	}
-
-	public void excute() {
-
-		int i = 2;
-		try {
-			// 查询数据库
-//			List<Object> objs = (List<Object>) dao.findByHSQL(hsql,
-//					clazz.newInstance());
-//			toJson(objs);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 	/**
@@ -303,165 +274,163 @@ public class ToJsonUtil {
 	 */
 	public static boolean isEntityObj(String clazzName) {
 
-		if (classNames != null) {
-			for (String className : classNames) {
-				System.out.println(className);
-				if (clazzName.equals(className))
-					return true;
-			}
+		if(clazzName.startsWith("ppms.domain")){
+			return true;
 		}
 		return false;
 	}
 
-	/**
-	 * 获取某包下所有类
-	 * 
-	 * @param packageName
-	 *            包名
-	 * @param isRecursion
-	 *            是否遍历子包
-	 * @return 类的完整名称
-	 */
-	public static Set<String> getClassName(String packageName,
-			boolean isRecursion) {
-		Set<String> classNames = null;
-		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		String packagePath = packageName.replace(".", "/");
-
-		URL url = loader.getResource(packagePath);
-		if (url != null) {
-			String protocol = url.getProtocol();
-			if (protocol.equals("file")) {
-				classNames = getClassNameFromDir(url.getPath(), packageName,
-						isRecursion);
-			} else if (protocol.equals("jar")) {
-				JarFile jarFile = null;
-				try {
-					jarFile = ((JarURLConnection) url.openConnection())
-							.getJarFile();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-
-				if (jarFile != null) {
-					getClassNameFromJar(jarFile.entries(), packageName,
-							isRecursion);
-				}
-			}
-		} else {
-			/* 从所有的jar包中查找包名 */
-			classNames = getClassNameFromJars(
-					((URLClassLoader) loader).getURLs(), packageName,
-					isRecursion);
-		}
-
-		return classNames;
-	}
-
-	/**
-	 * 从项目文件获取某包下所有类
-	 * 
-	 * @param filePath
-	 *            文件路径
-	 * @param className
-	 *            类名集合
-	 * @param isRecursion
-	 *            是否遍历子包
-	 * @return 类的完整名称
-	 */
-	private static Set<String> getClassNameFromDir(String filePath,
-			String packageName, boolean isRecursion) {
-
-		Set<String> className = new HashSet<String>();
-		File file = new File(filePath);
-		File[] files = file.listFiles();
-		for (File childFile : files) {
-			if (childFile.isDirectory()) {
-				if (isRecursion) {
-					className.addAll(getClassNameFromDir(childFile.getPath(),
-							packageName + "." + childFile.getName(),
-							isRecursion));
-				}
-			} else {
-				String fileName = childFile.getName();
-				if (fileName.endsWith(".class") && !fileName.contains("$")) {
-					className.add(packageName + "."
-							+ fileName.replace(".class", ""));
-				}
-			}
-		}
-		return className;
-	}
-
-	/**
-	 * @param jarEntries
-	 * @param packageName
-	 * @param isRecursion
-	 * @return
-	 */
-	private static Set<String> getClassNameFromJar(
-			Enumeration<JarEntry> jarEntries, String packageName,
-			boolean isRecursion) {
-		Set<String> classNames = new HashSet<String>();
-
-		while (jarEntries.hasMoreElements()) {
-			JarEntry jarEntry = jarEntries.nextElement();
-			if (!jarEntry.isDirectory()) {
-				/*
-				 * 这里是为了方便，先把"/" 转成 "." 再判断 ".class" 的做法可能会有bug (FIXME: 先把"/" 转成
-				 * "." 再判断 ".class" 的做法可能会有bug)
-				 */
-				String entryName = jarEntry.getName().replace("/", ".");
-				if (entryName.endsWith(".class") && !entryName.contains("$")
-						&& entryName.startsWith(packageName)) {
-					entryName = entryName.replace(".class", "");
-					if (isRecursion) {
-						classNames.add(entryName);
-					} else if (!entryName.replace(packageName + ".", "")
-							.contains(".")) {
-						classNames.add(entryName);
-					}
-				}
-			}
-		}
-		return classNames;
-	}
-
-	/**
-	 * 从所有jar中搜索该包，并获取该包下所有类
-	 * 
-	 * @param urls
-	 *            URL集合
-	 * @param packageName
-	 *            包路径
-	 * @param isRecursion
-	 *            是否遍历子包
-	 * @return 类的完整名称
-	 */
-	private static Set<String> getClassNameFromJars(URL[] urls,
-			String packageName, boolean isRecursion) {
-		Set<String> classNames = new HashSet<String>();
-
-		for (int i = 0; i < urls.length; i++) {
-			String classPath = urls[i].getPath();
-
-			// 不必搜索classes文件夹
-			if (classPath.endsWith("classes/")) {
-				continue;
-			}
-
-			JarFile jarFile = null;
-			try {
-				jarFile = new JarFile(classPath.substring(classPath
-						.indexOf("/")));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			if (jarFile != null) {
-				classNames.addAll(getClassNameFromJar(jarFile.entries(),
-						packageName, isRecursion));
-			}
-		}
-		return classNames;
-	}
+	
+	
+//	/**
+//	 * 获取某包下所有类
+//	 * 
+//	 * @param packageName
+//	 *            包名
+//	 * @param isRecursion
+//	 *            是否遍历子包
+//	 * @return 类的完整名称
+//	 */
+//	public static Set<String> getClassName(String packageName,
+//			boolean isRecursion) {
+//		Set<String> classNames = null;
+//		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+//		String packagePath = packageName.replace(".", "/");
+//
+//		URL url = loader.getResource(packagePath);
+//		if (url != null) {
+//			String protocol = url.getProtocol();
+//			if (protocol.equals("file")) {
+//				classNames = getClassNameFromDir(url.getPath(), packageName,
+//						isRecursion);
+//			} else if (protocol.equals("jar")) {
+//				JarFile jarFile = null;
+//				try {
+//					jarFile = ((JarURLConnection) url.openConnection())
+//							.getJarFile();
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//
+//				if (jarFile != null) {
+//					getClassNameFromJar(jarFile.entries(), packageName,
+//							isRecursion);
+//				}
+//			}
+//		} else {
+//			/* 从所有的jar包中查找包名 */
+//			classNames = getClassNameFromJars(
+//					((URLClassLoader) loader).getURLs(), packageName,
+//					isRecursion);
+//		}
+//
+//		return classNames;
+//	}
+//
+//	/**
+//	 * 从项目文件获取某包下所有类
+//	 * 
+//	 * @param filePath
+//	 *            文件路径
+//	 * @param className
+//	 *            类名集合
+//	 * @param isRecursion
+//	 *            是否遍历子包
+//	 * @return 类的完整名称
+//	 */
+//	private static Set<String> getClassNameFromDir(String filePath,
+//			String packageName, boolean isRecursion) {
+//
+//		Set<String> className = new HashSet<String>();
+//		File file = new File(filePath);
+//		File[] files = file.listFiles();
+//		for (File childFile : files) {
+//			if (childFile.isDirectory()) {
+//				if (isRecursion) {
+//					className.addAll(getClassNameFromDir(childFile.getPath(),
+//							packageName + "." + childFile.getName(),
+//							isRecursion));
+//				}
+//			} else {
+//				String fileName = childFile.getName();
+//				if (fileName.endsWith(".class") && !fileName.contains("$")) {
+//					className.add(packageName + "."
+//							+ fileName.replace(".class", ""));
+//				}
+//			}
+//		}
+//		return className;
+//	}
+//
+//	/**
+//	 * @param jarEntries
+//	 * @param packageName
+//	 * @param isRecursion
+//	 * @return
+//	 */
+//	private static Set<String> getClassNameFromJar(
+//			Enumeration<JarEntry> jarEntries, String packageName,
+//			boolean isRecursion) {
+//		Set<String> classNames = new HashSet<String>();
+//
+//		while (jarEntries.hasMoreElements()) {
+//			JarEntry jarEntry = jarEntries.nextElement();
+//			if (!jarEntry.isDirectory()) {
+//				/*
+//				 * 这里是为了方便，先把"/" 转成 "." 再判断 ".class" 的做法可能会有bug (FIXME: 先把"/" 转成
+//				 * "." 再判断 ".class" 的做法可能会有bug)
+//				 */
+//				String entryName = jarEntry.getName().replace("/", ".");
+//				if (entryName.endsWith(".class") && !entryName.contains("$")
+//						&& entryName.startsWith(packageName)) {
+//					entryName = entryName.replace(".class", "");
+//					if (isRecursion) {
+//						classNames.add(entryName);
+//					} else if (!entryName.replace(packageName + ".", "")
+//							.contains(".")) {
+//						classNames.add(entryName);
+//					}
+//				}
+//			}
+//		}
+//		return classNames;
+//	}
+//
+//	/**
+//	 * 从所有jar中搜索该包，并获取该包下所有类
+//	 * 
+//	 * @param urls
+//	 *            URL集合
+//	 * @param packageName
+//	 *            包路径
+//	 * @param isRecursion
+//	 *            是否遍历子包
+//	 * @return 类的完整名称
+//	 */
+//	private static Set<String> getClassNameFromJars(URL[] urls,
+//			String packageName, boolean isRecursion) {
+//		Set<String> classNames = new HashSet<String>();
+//
+//		for (int i = 0; i < urls.length; i++) {
+//			String classPath = urls[i].getPath();
+//
+//			// 不必搜索classes文件夹
+//			if (classPath.endsWith("classes/")) {
+//				continue;
+//			}
+//
+//			JarFile jarFile = null;
+//			try {
+//				jarFile = new JarFile(classPath.substring(classPath
+//						.indexOf("/")));
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//			if (jarFile != null) {
+//				classNames.addAll(getClassNameFromJar(jarFile.entries(),
+//						packageName, isRecursion));
+//			}
+//		}
+//		return classNames;
+//	}
 }
