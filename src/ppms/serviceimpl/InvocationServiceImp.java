57d2871298e6ced9d756f0350fdb6eee58d97b36
SharksevenRo <937257166@qq.com>
2015-08-17 09:02:17 +0800

    级联操作简化 api

--- src/ppms/serviceimpl/InvocationServiceImp.java
+++ src/ppms/serviceimpl/InvocationServiceImp.java
@@ -2,9 +2,15 @@ package ppms.serviceimpl;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.impl.CriteriaImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.sun.org.apache.bcel.internal.generic.GETSTATIC;
+
+import ppms.daoimpl.BaseDaoImp;
 import ppms.daoimpl.InvocationDaoImp;
 import ppms.domain.OrganizationNj;
 import ppms.domain.TbInnovation;
@@ -15,35 +21,45 @@ import ppms.service.InvocationService;
 @Service
 public class InvocationServiceImp implements InvocationService {
 
-	public InvocationServiceImp(){
-		
+	public InvocationServiceImp() {
+
 		System.out.println("create");
 	}
+
 	@Autowired
 	private TbInnovationDAO dao;
 	@Autowired
 	private OrganizationNjDAO orgDao;
+	@Autowired
+	@Qualifier("baseDaoImp")
+	private BaseDaoImp baseDao;
+
 	@Override
 	public List<OrganizationNj> getOrganizations() {
-		
+
 		return orgDao.findAll();
 	}
+
 	@Override
 	public boolean addInnovation(TbInnovation innovation) {
-		
+
 		try {
 			dao.saveObject(innovation);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return false;
 	}
+
 	@Override
 	public List<TbInnovation> findAllInnovations() {
-		
-		String hsql="from TbInnovation inn,TbEmployee emp,OrganizationNj org where inn.employeeid=emp.employeeid and inn.orgid=org.orgid";
-		return dao.findbyHsql(hsql);
+
+		// String hsql="from TbInnovation fetch all properties";
+		// return dao.findbyHsql(hsql);
+
+		return baseDao.getEntitiestNotLazy(new TbInnovation(), new String[] {
+				"organizationNj", "tbEmployee" });
 	}
-	
+
 }
